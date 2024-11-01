911fe8135dfa1b3c878efb92d4517439246eb6f7
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-11-01 15:48:41 +0530

    Update authConfig.js

--- backend/config/authConfig.js
+++ backend/config/authConfig.js
@@ -1,1 +1,11 @@
+// backend/config/authConfig.js
+
+const dotenv = require('dotenv');
+// Load environment variables from .env file
+dotenv.config();
+
+module.exports = {
+    jwtSecret: process.env.JWT_SECRET || 'your-default-secret-key', // Secret key for JWT
+    jwtExpiration: process.env.JWT_EXPIRATION || '1d', // Token expiration time (default: 1 day)
+};
 
