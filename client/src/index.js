6b7d40fb73c7e613152ee8ca15be23f4a395be79
Samyak Mishra <115927401+Samyak072@users.noreply.github.com>
2024-11-01 11:55:05 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,14 @@
+// client/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
